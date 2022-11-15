cd7fae7aa38606eb8cbfdc7eca9958caa403f091
Emmanuel Oladipupo <manueltech401@gmail.com>
2022-11-15 19:35:35 +0100

    designed the metabnb page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 
 import './App.css';
+import MetaBnBCard from './components/MetaBnBCard';
+import MetaPage from './components/MetaPage';
 
 function App() {
   return (
-    <div className="">
-      
-    </div>
+    <MetaPage />
   );
 }
 
---
+++ src/components/MetaHeaderStyled.js
@@ -0,0 +1,20 @@
+import styled from "styled-components";
+
+export const Header = styled.div`
+
+display: flex;
+flex-direction: row;
+/* justify-content: space-around; */
+/* align-items: center; */
+border: 1px solid yellow;
+overflow: hidden;
+
+width: 100%;
+height: 70px;
+background: linear-gradient(90deg, #A02279 11.45%, #A02279 11.45%);
+
+/* .header-image{
+    width: 222px;
+
+} */
+`
